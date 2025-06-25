5bb3c0bade4d09c1f0d4907dead703de610264fd
Richie <20235894@esen.edu.sv>
2025-06-24 18:54:39 -0600

    feat: add Auth, Users, and Courses modules to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
